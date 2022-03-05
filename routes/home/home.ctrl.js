81989875a0f698a0e1f007053dfdaff7f9befec2
grang <shee0201@naver.com>
2022-03-06 00:06:02 +0900

    깃 저자아소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const home = (req,res) => {
+  res.render("home/index");
+};
+
+function login(req,res) {
+  res.render("home/login");
+}
+
+module.exports={
+  home,
+  login,
+};
+

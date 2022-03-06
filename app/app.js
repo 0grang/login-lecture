8dd721f210fb48d25bd20bd3224e6ccee7dd1d57
grang <shee0201@naver.com>
2022-03-06 14:48:47 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
 app.set("views","./views");

6b9f46cf8fec987cf4e18e3f779c000aeb787fd9
grang <shee0201@naver.com>
2022-03-07 16:54:38 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -4,8 +4,9 @@ const PORT = 3000;
 const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
+app.set(express.static(`${__dirname}/src/public`));
 
 
 app.use("/", home); //use - >미들 웨어를 등록해주는 메서드
